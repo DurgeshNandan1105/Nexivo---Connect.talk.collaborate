9f3357c8929925c26ac838d97e36de427e3c8b07
Durgesh Nandan <durgeshnandan748@gmail.com>
2026-09-25 00:16:38 +0530

    adding socket.io function

--- backend/src/app.js
+++ backend/src/app.js
@@ -7,6 +7,7 @@ import dotenv from "dotenv";
 dotenv.config();
 import cors from "cors";
 import connectToSocket from './controllers/socketManager.js';
+import userRoutes from "./routes/users.routes.js";
 
 const app = express();
 const server = createServer(app);
@@ -15,6 +16,7 @@ app.set("port", (process.env.PORT || 8000));
 app.use(cors());
 app.use(express.json({limit: "40kb"}));
 app.use(express.urlencoded({limit: "40kb", extended: true}));
+app.use("/api/v1/users", userRoutes);
 
 
 
