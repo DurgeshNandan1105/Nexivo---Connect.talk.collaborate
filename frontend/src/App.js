9ee4b4d7fada9ff101e3e0e8a8641c959d118ba5
Durgesh Nandan <durgeshnandan748@gmail.com>
2026-09-26 00:17:13 +0530

    adding frontend

---
+++ frontend/src/App.js
@@ -0,0 +1,24 @@
+
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import './App.css';
+import LandingPage from './pages/landing';
+
+function App() {
+  return (
+    <div className='App'>
+   <>
+   <Router>
+    <Routes>
+      {/* <Route path="/home" element=/>  */}
+      <Route path='/' element={<LandingPage/>} />
+
+    </Routes>
+
+   </Router>
+   
+   </>
+   </div>
+  );
+}
+
+export default App;
